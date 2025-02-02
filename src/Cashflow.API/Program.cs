680b04acd761684d77a3710492c2526414754819
Caio Almeida <caioalmeidanaweb@gmail.com>
2025-02-02 17:40:28 -0400

    second commit

---
+++ src/Cashflow.API/Program.cs
@@ -0,0 +1,28 @@
+using Cashflow.API.Filters;
+using Cashflow.Application;
+using Cashflow.Infra;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+builder.Services.AddInfra(builder.Configuration);
+builder.Services.AddApplication();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+
+
+app.Run();
